bd53b3b98d480badf0757f89f6947838d02697ee
getsadzeg <getsadzeguram@gmail.com>
2017-03-04 12:42:45 +0400

    trying to validate user

---
+++ src/java/ge/mziuri/exceptions/RegistrationFailedException.java
@@ -0,0 +1,15 @@
+
+package ge.mziuri.exceptions;
+
+
+public class RegistrationFailedException extends Exception {
+
+    
+    public RegistrationFailedException() {
+    }
+
+    
+    public RegistrationFailedException(String msg) {
+        super(msg);
+    }
+}
--- src/java/ge/mziuri/servlet/RegisterServlet.java
+++ src/java/ge/mziuri/servlet/RegisterServlet.java
@@ -4,13 +4,13 @@ package ge.mziuri.servlet;
 import ge.mziuri.dao.UserDAO;
 import ge.mziuri.dao.UserDAOImpl;
 import ge.mziuri.model.User;
+import ge.mziuri.exceptions.RegistrationFailedException;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import sun.security.provider.MD5;
 
 public class RegisterServlet extends HttpServlet {
     
@@ -25,11 +25,28 @@ public class RegisterServlet extends HttpServlet {
         String lastName = request.getParameter("lastName");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        /* User user = new User(firstName, lastName, username, String.valueOf(password.hashCode()));
-        UserDAO userDAO = new UserDAOImpl();
-        userDAO.register(user); */
-        RequestDispatcher rd = request.getRequestDispatcher("index.html"); //needs front branch
+        String repeatedPassword = request.getParameter("password2");
+        try {
+            validate(firstName, lastName, username, password, repeatedPassword);
+            //construct here
+            //userDAO impl new object here
+            //userDAO.register(user);
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        RequestDispatcher rd = request.getRequestDispatcher("login.html"); 
         rd.forward(request, response);
+    }
+    
+    public void validate(String firstName, String lastName, String username, 
+            String password, String repeatedPassword) throws RegistrationFailedException {
+        if(firstName.length() > 30) throw new RegistrationFailedException("თქვენი სახელი არ უნდა აღემატებოდეს 30 სიმბოლოს");
+        if(firstName.length() > 30) throw new RegistrationFailedException("თქვენი გვარი არ უნდა აღემატებოდეს 30 სიმბოლოს");
+        if(username.length() > 30) throw new RegistrationFailedException("მომხმარებლის სახელი არ უნდა აღემატებოდეს 30 სიმბოლოს");
+        if(password.length() > 30 || password.length() < 6) throw new RegistrationFailedException("მომხმარებლის სახელი არ უნდა აღემატებოდეს 30 სიმბოლოს"
+                + "და უნდა იყოს 6 სიმბოლოზე მეტი");
     }
     
 }
