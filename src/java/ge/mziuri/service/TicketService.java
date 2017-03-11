35a8c22c95bdcf259b0fe84c17b79a9b28153820
getsadzeg <getsadzeguram@gmail.com>
2017-03-11 16:45:40 +0400

    event author is now getting sold tickets' money

--- src/java/ge/mziuri/service/TicketService.java
+++ src/java/ge/mziuri/service/TicketService.java
@@ -7,6 +7,7 @@ import ge.mziuri.dao.DatabaseUtil;
 import ge.mziuri.model.Card;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -18,24 +19,46 @@ public class TicketService {
         con = DatabaseUtil.getConnection();
     }
     public void processPayment(int EventID, int userID) {
+        ResultSet result = null;
         Card card = null;
         try {
-            pstmt = con.prepareStatement("SELECT price FROM EVENT where id = ?");
+            pstmt = con.prepareStatement("SELECT price, author_username FROM EVENT where id = ?");
             pstmt.setInt(1, EventID);
-            double price = pstmt.executeQuery().getDouble("price");
+            result = pstmt.executeQuery();
+            double price = result.getDouble("price");
+            String author_username = result.getString("author_username");
             pstmt = con.prepareStatement("SELECT card_id FROM USER WHERE id = ?");
             pstmt.setInt(1, userID);
             int card_id = pstmt.executeQuery().getInt("card_id");
-            pstmt = con.prepareStatement("SELECT money FROM CARD WHERE id = ?");
-            pstmt.setInt(1, card_id);
-            int money = pstmt.executeQuery().getInt("money");
+            int money = moneywithCardID(card_id);
             card.setMoney(money);
             card.setId(card_id);
             CardDAO cardDAO = new CardDAOImpl();
             cardDAO.updateMoney(money-price, card);
+            pstmt = con.prepareStatement("SELECT card_id FROM USER WHERE author_username = ?");
+            int author_card_id = pstmt.executeQuery().getInt("card_id");
+            int author_money = moneywithCardID(author_card_id);
+            card.setMoney(author_money);
+            card.setId(author_card_id);
+            cardDAO.updateMoney(author_money+price*(9/10), card);
         }
         catch(SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
+    
+    public int moneywithCardID(int card_id) {
+        int money;
+        try {
+            pstmt = con.prepareStatement("SELECT money FROM CARD WHERE id = ?");
+            pstmt.setInt(1, card_id);
+            money = pstmt.executeQuery().getInt("money");
+        }
+        catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return money;
+    }
+    
+    
 }
