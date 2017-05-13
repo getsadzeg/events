c3a95364976f106fc9dea4deaa6a379c4e12d24d
getsadzeg <getsadzeguram@gmail.com>
2017-05-13 15:25:23 +0400

    catching exception if user is not logined and trying to buy ticket

--- src/java/ge/mziuri/servlet/BuyTicketServlet.java
+++ src/java/ge/mziuri/servlet/BuyTicketServlet.java
@@ -4,6 +4,7 @@ import ge.mziuri.dao.TicketDAO;
 import ge.mziuri.dao.TicketDAOImpl;
 import ge.mziuri.util.CookieUtil;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,24 +19,27 @@ public class BuyTicketServlet extends HttpServlet {
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
-        TicketDAO ticketDAO = new TicketDAOImpl();
-        String eventID = "";
-        String userID = "";
-        eventID = CookieUtil.getDataFromRequest("eventIDCookie", request);
-        userID = CookieUtil.getDataFromRequest("userIDCookie", request);
-        int selection = Integer.parseInt(request.getParameter("selection"));
-        ticketDAO.buyTicket(Integer.parseInt(eventID), Integer.parseInt(userID), selection); 
-        
-        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
+            
+            TicketDAO ticketDAO = new TicketDAOImpl();
+            String eventID = "";
+            String userID = "";
+            eventID = CookieUtil.getDataFromRequest("eventIDCookie", request);
+            userID = CookieUtil.getDataFromRequest("userIDCookie", request);
+            int selection = Integer.parseInt(request.getParameter("selection"));
+            ticketDAO.buyTicket(Integer.parseInt(eventID), Integer.parseInt(userID), selection);
+
+            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+
             rd.forward(request, response);
-        }
-        
-        catch(ServletException | IOException ex) {
+        } catch (ServletException | IOException | NumberFormatException ex) {
+            response.setContentType("text/html");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter pt = response.getWriter();
+            pt.append("დაფიქსირდა შეცდომა");
             System.out.println(ex.getMessage());
         }
     }
 
 }
-
